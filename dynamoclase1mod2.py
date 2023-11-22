351edaa30a26715f9774b05c2f7534524c6994d7
EC2 Default User <ec2-user@ip-172-31-11-29.ec2.internal>
2023-11-22 15:25:05 +0000

    hola

--- dynamoclase1mod2.py
+++ dynamoclase1mod2.py
@@ -4,5 +4,19 @@ def listar_tablas():
     dynamodb = boto3.resource('dynamodb')
     for table in dynamodb.tables.all():
         print(table.name)
+  
+  opcion =  1
+  
+  while opcion 1 = 0:
+      print("Ingrese la opc que desea")
+      print("1.listar tablas de dyanmodb de la cuenta")
+      print("0. Salir")
+      opcion =int(input())
+    if opcion == 1:
+      listar_tablas()
+    elif opcion  == 0:
+      print ("Hasta luego")
+    else:  
+      print("opcion no valida ")
         
 listar_tablas()
